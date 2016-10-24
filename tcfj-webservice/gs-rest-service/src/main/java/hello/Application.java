f6b84e31cd346f1105f0f5808c753dae4a1c158a
mks3251 <mksloan6@gmail.com>
2016-10-23 19:21:16 -0500

    Add files via upload

---
+++ tcfj-webservice/gs-rest-service/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Created by MK on 10/23/2016.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
